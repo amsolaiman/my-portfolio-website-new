42769f091b7efa4c52e0e5a1001dd7ed7bbd140e
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-11-18 14:21:40 +0800

    chore: rename everything home --> hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 // layout
 import Layout from "@/layout";
 // sections
-import HomeView from "@/sections/home/view";
+import HeroView from "@/sections/hero/view";
 
 // ----------------------------------------------------------------------
 
 export default function Page() {
   return (
     <Layout>
-      <HomeView />
+      <HeroView />
 
       <div className="h-[200vh]" />
     </Layout>
