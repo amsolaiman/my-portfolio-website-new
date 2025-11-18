ad00b65cd19b27e65127f60ab35fafaabd038cec
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-11-18 09:59:27 +0800

    chore: redesigning home section

--- src/sections/home/home-navigation.tsx
+++ src/sections/home/home-navigation.tsx
@@ -6,29 +6,37 @@ export default function HomeNavigation() {
   const LINKS = [
     {
       label: "About",
-      href: "/about",
+      href: "#about",
     },
     {
       label: "Experience",
-      href: "/experience",
+      href: "#experience",
     },
     {
       label: "Works",
-      href: "/works",
+      href: "#works",
     },
   ];
 
   return (
-    <div className="flex gap-20 items-center p-4">
-      {LINKS.map((link) => (
-        <Link
-          key={link.label}
-          href={link.href}
-          className="text-lg/4 uppercase tracking-widest font-light hover:opacity-70 transition-opacity"
-        >
-          {link.label}
-        </Link>
-      ))}
+    <div className="flex justify-between">
+      <div className="flex flex-col gap-2">
+        {LINKS.map((link) => (
+          <Link
+            key={link.label}
+            href={link.href}
+            className="text-xs uppercase font-light hover:opacity-70 transition-opacity"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </div>
+
+      <p className="text-xs uppercase font-light">
+        {["Frontend Developer", "UI/UX Designer", "Graphic Designer"]
+          .map((_) => _)
+          .join(" / ")}
+      </p>
     </div>
   );
 }
--- src/sections/home/view/home-view.tsx
+++ src/sections/home/view/home-view.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+//
 import HomeFooter from "../home-footer";
 import HomeNavigation from "../home-navigation";
 
@@ -5,13 +7,21 @@ import HomeNavigation from "../home-navigation";
 
 export default function HomeView() {
   return (
-    <section
-      id="home"
-      className="home-background w-full h-svh flex items-center justify-center text-background overflow-hidden"
-    >
-      <HomeNavigation />
+    <section id="home" className="w-full h-svh flex flex-col">
+      <Image
+        src="/assets/hero_brand_name.svg"
+        alt="Hero Brand"
+        width={1280}
+        height={125}
+        draggable={false}
+        className="w-full h-fit object-top object-cover"
+      />
 
-      <HomeFooter />
+      <div className="home-background w-full p-4 grow flex flex-col justify-between">
+        <HomeNavigation />
+
+        <HomeFooter />
+      </div>
     </section>
   );
 }
