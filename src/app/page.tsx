dce6bfd365965614e8a1bd9b741580482c06c85c
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-11-19 14:36:04 +0800

    chore: restructure home section files & folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Layout from "@/layout";
 // sections
 import AboutView from "@/sections/about";
-import HeroView from "@/sections/hero/view";
+import HeroView from "@/sections/hero";
 
 // ----------------------------------------------------------------------
 
