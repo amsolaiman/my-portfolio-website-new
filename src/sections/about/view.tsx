8405d70443792de60d34c53f8841c199115c4407
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2025-11-19 14:22:37 +0800

    feat: add about section (#21)

---
+++ src/sections/about/view.tsx
@@ -0,0 +1,31 @@
+// ----------------------------------------------------------------------
+
+export default function AboutView() {
+  const SKILLS = [
+    "React / Next.js",
+    "TypeScript",
+    "Tailwind-CSS",
+    "Figma",
+    "React Native",
+    "GraphQL",
+    "Material-UI",
+    "Photoshop / Illustrator",
+  ];
+
+  return (
+    <section id="about" className="pt-24 px-12">
+      <h3 className="w-5/6 max-w-250 text-2xl font-light uppercase [&>strong]:font-medium [&>em]:not-italic [&>em]:opacity-50">
+        Hey! I&apos;m <strong>Moiz Solaiman</strong>, a frontend developer and
+        designer based in the Philippines.&nbsp;
+        <em>
+          With 3+ years of experience taking part in webdev projects for local
+          and international clients.
+        </em>
+      </h3>
+
+      <p className="mt-4 text-xs font-light uppercase">
+        {SKILLS.map((_) => _).join(" | ")}
+      </p>
+    </section>
+  );
+}
