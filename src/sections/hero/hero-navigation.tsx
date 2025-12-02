70894d1e1067e19978ec9ab659c2878021bab2e2
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-12-02 14:32:01 +0800

    chore: apply hero section mobile responsiveness

--- src/sections/hero/hero-navigation.tsx
+++ src/sections/hero/hero-navigation.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 // ----------------------------------------------------------------------
 
 export default function HeroNavigation() {
+  const ROLES = ["Frontend Developer", "UI/UX Designer", "Graphic Designer"];
+
   const LINKS = [
     {
       label: "About",
@@ -21,8 +23,8 @@ export default function HeroNavigation() {
   ];
 
   return (
-    <div className="flex justify-between">
-      <div className="flex flex-col gap-2">
+    <div className="flex justify-between items-start">
+      <div className="hidden md:flex flex-col gap-2">
         {LINKS.map((link) => (
           <Link
             key={link.label}
@@ -44,11 +46,24 @@ export default function HeroNavigation() {
         ))}
       </div>
 
-      <p className="text-xs uppercase font-light">
-        {["Frontend Developer", "UI/UX Designer", "Graphic Designer"]
-          .map((_) => _)
-          .join(" / ")}
+      <button
+        type="button"
+        className="cursor-pointer md:hidden text-xs uppercase font-light hover:opacity-70 transition-opacity"
+      >
+        Menu
+      </button>
+
+      <p className="hidden md:block text-xs uppercase font-light">
+        {ROLES.map((_) => _).join(" / ")}
       </p>
+
+      <div className="flex md:hidden flex-col items-end gap-2">
+        {ROLES.map((_) => (
+          <p key={_} className="text-xs uppercase font-light">
+            {_}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
