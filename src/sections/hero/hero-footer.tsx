07e2f00be98d4c835901dff03f569e4dd491f0fe
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2025-12-03 07:00:48 +0800

    chore: apply hero section mobile responsiveness (#30)

--- src/sections/hero/hero-footer.tsx
+++ src/sections/hero/hero-footer.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function HeroFooter() {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-center md:justify-between">
       <p className="text-xs font-light uppercase opacity-50">Scoll down</p>
 
       <Link
         href="/"
-        className="text-xs font-light uppercase hover:opacity-70 transition-opacity"
+        className="hidden md:block text-xs font-light uppercase hover:opacity-70 transition-opacity"
       >
         Download Resume
       </Link>
--- src/sections/hero/view.tsx
+++ src/sections/hero/view.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Image from "next/image";
+// hooks
+import { useResponsive } from "@/hooks/use-responsive";
 //
 import HeroFooter from "./hero-footer";
 import HeroNavigation from "./hero-navigation";
@@ -6,18 +10,24 @@ import HeroNavigation from "./hero-navigation";
 // ----------------------------------------------------------------------
 
 export default function HeroView() {
+  const upLg = useResponsive("up", "lg");
+
   return (
     <section id="hero" className="w-full h-svh flex flex-col">
       <Image
-        src="/assets/hero_brand_name.svg"
+        src={
+          upLg
+            ? "/assets/hero_brand_name_desktop.svg"
+            : "/assets/hero_brand_name_mobile.svg"
+        }
         alt="Hero Brand"
         width={1280}
         height={125}
         draggable={false}
-        className="-mt-1 w-full h-fit object-top object-cover"
+        className="lg:-mt-1 w-full h-fit object-top object-cover"
       />
 
-      <div className="hero-background w-full p-4 grow flex flex-col justify-between">
+      <div className="hero-background w-full px-2 md:px-4 py-4 grow flex flex-col justify-between">
         <HeroNavigation />
 
         <HeroFooter />
