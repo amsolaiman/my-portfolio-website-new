2c1ef93dc0f1ec55d13c75e15fe3f411fdd2a236
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-12-05 14:25:33 +0800

    chore: apply mobile responsiveness to works section

--- src/sections/work/view.tsx
+++ src/sections/work/view.tsx
@@ -1,20 +1,28 @@
+"use client";
+
 // data
 import projects from "@/_store/project";
+// hooks
+import { useResponsive } from "@/hooks/use-responsive";
 
 // ----------------------------------------------------------------------
 
 export default function WorkView() {
+  const upMd = useResponsive("up", "md");
+
   return (
-    <section id="work" className="px-4 pt-24">
-      <div className="flex mb-4 justify-end">
+    <section id="work" className="px-2 md:px-4 pt-24">
+      <div className="flex mb-4 justify-end gap-2">
         <h5 className="text-xl uppercase">
           [{String(projects.length).padStart(2, "0")}]
         </h5>
 
-        <h1 className="text-7xl font-sans uppercase">Selected Works</h1>
+        <h1 className="text-7xl font-sans uppercase">
+          {upMd ? "Selected Works" : "Works"}
+        </h1>
       </div>
 
-      <div className="w-full border border-foreground/70 grid grid-cols-3 divide-x divide-y divide-foreground/70">
+      <div className="w-full border border-foreground/70 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 divide-x divide-y divide-foreground/70">
         {projects.map((project, index) => (
           <div
             key={project.name + index}
